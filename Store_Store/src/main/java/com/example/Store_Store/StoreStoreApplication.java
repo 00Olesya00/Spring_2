28c6de23e38afb98e83be9907b62c0722f279dd9
Meshcheryakova <lmavi.kisune@gmail.com>
2023-03-06 16:10:06 +0400

    Initial commit

---
+++ Store_Store/src/main/java/com/example/Store_Store/StoreStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.Store_Store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreStoreApplication.class, args);
+	}
+
+}
---
+++ Store_Store/src/test/java/com/example/Store_Store/StoreStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Store_Store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
